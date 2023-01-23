033218987e8c23ebf3357f427dd79de229a3af7e
clariedm <43651294+clariedm@users.noreply.github.com>
2023-01-23 11:52:54 +0100

    Update Executable2.sh

--- Executable2.sh
+++ Executable2.sh
@@ -5,7 +5,7 @@ echo Defining reboot parameters ...
 cd .config
 sudo mkdir -p lxsession/LXDE-pi
 cd 
-mv /bin/autostart /.config/lxsession/LXDE-pi/autostart
+mv bin/autostart /.config/lxsession/LXDE-pi/autostart
 echo All done ! Rebooting in 10 seconds.
 sleep 10
 sudo reboot
