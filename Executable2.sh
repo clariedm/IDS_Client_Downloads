35429cd9a6871eeda943425acec25e3e943975f0
clariedm <43651294+clariedm@users.noreply.github.com>
2023-01-23 15:16:41 +0100

    Add files via upload

--- Executable2.sh
+++ Executable2.sh
@@ -2,10 +2,11 @@
 echo Setting wallpaper ...
 pcmanfm --set-wallpaper /bin/Wallpaper.png
 echo Defining reboot parameters ...
-cd .config
+cd
+cd /home/pi/.config
 sudo mkdir -p lxsession/LXDE-pi
 cd 
-mv bin/autostart /.config/lxsession/LXDE-pi/autostart
+mv bin/autostart /home/pi/.config/lxsession/LXDE-pi/autostart
 echo All done ! Rebooting in 10 seconds.
 sleep 10
 sudo reboot
