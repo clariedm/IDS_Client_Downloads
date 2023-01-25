727f38aa62802ec3528670da48d2e9f0ea65126e
clariedm <43651294+clariedm@users.noreply.github.com>
2023-01-25 11:25:37 +0100

    Update openchromium2.sh

--- openchromium2.sh
+++ openchromium2.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 sleep 30
 sudo killall chromium-browser
-chromium-browser --start-fullscreen --start-maximized 127.0.0.1:1880/ui
+chromium-browser --noerrdialogs --start-fullscreen --start-maximized 127.0.0.1:1880/ui
