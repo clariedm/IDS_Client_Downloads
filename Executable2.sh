ed3fc49349f0c58be35d3cad216694b807d5cda7
clariedm <43651294+clariedm@users.noreply.github.com>
2023-01-25 10:00:31 +0100

    Update Executable2.sh

--- Executable2.sh
+++ Executable2.sh
@@ -7,6 +7,7 @@ cd /home/pi/.config
 sudo mkdir -p lxsession/LXDE-pi
 cd 
 sudo mv /home/pi/bin/autostart /home/pi/.config/lxsession/LXDE-pi/autostart
+sudo mv /home/pi/bin/desktop-items-0.conf /etc/xdg/pcmanfm/LXDE-pi/
 echo All done ! Rebooting in 10 seconds.
 sleep 10
 sudo reboot
