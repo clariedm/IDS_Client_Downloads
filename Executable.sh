af04ceabd20ce810045f5896e44def447c2df0c5
clariedm <43651294+clariedm@users.noreply.github.com>
2023-01-27 15:52:39 +0100

    Update Executable.sh

--- Executable.sh
+++ Executable.sh
@@ -12,12 +12,12 @@ echo Installing NodeRed ...
 sudo apt install build-essential git curl
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
 echo Configuring NodeRed ...
-sudo npm install -g pm2
-pm2 start /usr/bin/node-red --node-args="--max-old-space-size=128" -- -v
-pm2 save
-pm2 startup systemd
+#sudo npm install -g pm2
+#pm2 start /usr/bin/node-red --node-args="--max-old-space-size=128" -- -v
+#pm2 save
+#pm2 startup systemd
 echo Configuring reboot parameters ...
-sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u pi --hp /home/pi
+#sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u pi --hp /home/pi
 echo Installing NodeRed packages ...
 cd
 cd .node-red
