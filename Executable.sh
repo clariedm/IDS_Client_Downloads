527a8f18eb6f24145be5d1d3229d0145d26c7bff
clariedm <43651294+clariedm@users.noreply.github.com>
2023-01-27 16:35:37 +0100

    Update Executable.sh

--- Executable.sh
+++ Executable.sh
@@ -7,22 +7,15 @@ sudo apt-get install bluetooth pi-bluetooth blueman
 echo Removing legacy nodejs and installing the latest version ...
 sudo apt-get remove nodejs nodejs-legacy -y
 sudo apt-get nodejs -y
-#curl -L https://git.io/n-install | bash
 echo Installing NodeRed ...
 sudo apt install build-essential git curl
 bash <(curl -sL https://raw.githubusercontent.com/node-red/linux-installers/master/deb/update-nodejs-and-nodered)
-echo Configuring NodeRed ...
-#sudo npm install -g pm2
-#pm2 start /usr/bin/node-red --node-args="--max-old-space-size=128" -- -v
-#pm2 save
-#pm2 startup systemd
-echo Configuring reboot parameters ...
-#sudo env PATH=$PATH:/usr/bin /usr/lib/node_modules/pm2/bin/pm2 startup systemd -u pi --hp /home/pi
 echo Installing NodeRed packages ...
 cd
 cd .node-red
 npm i node-red-dashboard
 npm i node-red-node-ui-table
+npm install node-red-contrib-pythonshell
 echo Moving files downloaded from github ...
 cd
 sudo mv -v /home/pi/Desktop/IDS_Client_Downloads /home/pi/bin
