b0aff2add408fa164f228e2ec221c517be25ca3a
clariedm <43651294+clariedm@users.noreply.github.com>
2023-01-27 10:44:11 +0100

    Update Executable2.sh

--- Executable2.sh
+++ Executable2.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
-echo Setting wallpaper ...
-pcmanfm --set-wallpaper home/pi/bin/Wallpaper.png
 echo Defining reboot parameters ...
 cd
 cd /home/pi/.config
@@ -10,6 +8,8 @@ sudo mv /home/pi/bin/autostart /home/pi/.config/lxsession/LXDE-pi/autostart
 cd
 sudo rm /etc/xdg/pcmanfm/LXDE-pi/desktop-items-0.conf
 sudo mv /home/pi/bin/desktop-items-0.conf /etc/xdg/pcmanfm/LXDE-pi/
+echo Setting wallpaper ...
+pcmanfm --set-wallpaper home/pi/bin/Wallpaper.png
 echo All done ! Rebooting in 10 seconds.
 sleep 10
 sudo reboot
