aac4ed38abbe3eba608c5c290dc3a18f5f0396c8
clariedm <43651294+clariedm@users.noreply.github.com>
2023-02-08 12:12:02 +0100

    Update Executable2.sh

--- Executable2.sh
+++ Executable2.sh
@@ -5,12 +5,6 @@ cd /home/pi/.config
 sudo mkdir -p lxsession/LXDE-pi # Creates the directories where the autostart file will be stored
 cd 
 sudo mv /home/pi/bin/autostart /home/pi/.config/lxsession/LXDE-pi/autostart # Moves the autostart file from bin to the newly created directories
-cd
-sudo sed -i "s/^show_trash.*/show_trash=0" /etc/xdg/pcmanfm/LXDE-pi/desktop-items-0.conf
-#sudo rm /etc/xdg/pcmanfm/LXDE-pi/desktop-items-0.conf # Deletes the existing desktop-items-0.conf file
-#sudo mv /home/pi/bin/desktop-items-0.conf /etc/xdg/pcmanfm/LXDE-pi/ # Moves the downloaded desktop-items-0.conf file to replace it
-#sudo rm /etc/xdg/pcmanfm/LXDE-pi/desktop-items-1.conf # Deletes the existing desktop-items-0.conf file
-#sudo mv /home/pi/bin/desktop-items-1.conf /etc/xdg/pcmanfm/LXDE-pi/ # Moves the downloaded desktop-items-0.conf file to replace it
 echo Setting wallpaper ...
 pcmanfm --set-wallpaper /home/pi/bin/Wallpaper.png 
 echo All done ! Rebooting in 10 seconds.
