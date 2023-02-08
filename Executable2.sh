1e83a61f15a2849a4e42c7cee55b5dbf63cef89f
clariedm <43651294+clariedm@users.noreply.github.com>
2023-02-08 14:56:07 +0100

    Update Executable2.sh

--- Executable2.sh
+++ Executable2.sh
@@ -4,6 +4,7 @@ cd
 cd /home/pi/.config
 sudo mkdir -p lxsession/LXDE-pi # Creates the directories where the autostart file will be stored
 sudo mkdir -p pcmanfm/LXDE-pi # Creates the directories where the pcmanfm profile files will be stored
+sudo mv /home/pi/bin/desktop-items-0.conf /home/pi/.config/pcmanfm/LXDE-pi/
 cd 
 sudo mv /home/pi/bin/autostart /home/pi/.config/lxsession/LXDE-pi/autostart # Moves the autostart file from bin to the newly created directories
 echo Setting wallpaper ...
