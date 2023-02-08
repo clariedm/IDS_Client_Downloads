7f841c162c2956c7ef021bd4ff0a585ecef4ca64
clariedm <43651294+clariedm@users.noreply.github.com>
2023-02-08 11:23:26 +0100

    Update Executable2.sh

--- Executable2.sh
+++ Executable2.sh
@@ -7,10 +7,10 @@ cd
 sudo mv /home/pi/bin/autostart /home/pi/.config/lxsession/LXDE-pi/autostart # Moves the autostart file from bin to the newly created directories
 cd
 sudo sed -i "s/^show_trash.*/show_trash=0" /etc/xdg/pcmanfm/LXDE-pi/desktop-items-0.conf
-sudo rm /etc/xdg/pcmanfm/LXDE-pi/desktop-items-0.conf # Deletes the existing desktop-items-0.conf file
-sudo mv /home/pi/bin/desktop-items-0.conf /etc/xdg/pcmanfm/LXDE-pi/ # Moves the downloaded desktop-items-0.conf file to replace it
-sudo rm /etc/xdg/pcmanfm/LXDE-pi/desktop-items-1.conf # Deletes the existing desktop-items-0.conf file
-sudo mv /home/pi/bin/desktop-items-1.conf /etc/xdg/pcmanfm/LXDE-pi/ # Moves the downloaded desktop-items-0.conf file to replace it
+#sudo rm /etc/xdg/pcmanfm/LXDE-pi/desktop-items-0.conf # Deletes the existing desktop-items-0.conf file
+#sudo mv /home/pi/bin/desktop-items-0.conf /etc/xdg/pcmanfm/LXDE-pi/ # Moves the downloaded desktop-items-0.conf file to replace it
+#sudo rm /etc/xdg/pcmanfm/LXDE-pi/desktop-items-1.conf # Deletes the existing desktop-items-0.conf file
+#sudo mv /home/pi/bin/desktop-items-1.conf /etc/xdg/pcmanfm/LXDE-pi/ # Moves the downloaded desktop-items-0.conf file to replace it
 echo Setting wallpaper ...
 pcmanfm --set-wallpaper /home/pi/bin/Wallpaper.png 
 echo All done ! Rebooting in 10 seconds.
